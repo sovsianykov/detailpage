1d5b40e8f5ae44c85aeef14984e4c5e64fa26aa7
sovsianykov <70006293+sovsianykov@users.noreply.github.com>
2020-12-07 00:48:47 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(function(){document.querySelector("#parent");document.querySelector(".cell").forEach(e=>{e.style.background="#9cb71e"})}));
+//# sourceMappingURL=script.js.map
